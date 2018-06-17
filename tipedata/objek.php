6e617a78f9ef03fd1e0a46ace8490529d82d2bba
Jaka Pratama <jakapratama204@gmail.com>
2018-06-17 21:24:25 +0700

    Revisi Belajar Objek

--- tipedata/objek.php
+++ tipedata/objek.php
@@ -4,21 +4,24 @@
  */
 class objek
 {
-	
+	// ini merupakan fungsi yang akan di eksekusi intrepeter pertama kali
 	function __construct()
 	{
 		# code...
 	}
-
+	// fungis untuk menampilkan kalimat 
 	function do_pesan(){
 		echo "Perkenalkan Saya Jaka";
 	}
 
+	// fungsi untuk memberikan nilai balikan 
 	public function setNilai(){
 		return 100;
 	}
 }
+// cara untuk inisiasi objek bernama 'objek'
 $objek = new objek();
 
+// cara pemamnggilan fungsi 
 echo $objek->do_pesan()."<br>";
 echo $objek->setNilai();
