f379c79fe6e588c24813f7289ac8190c9077759c
Jaka Pratama <jakapratama204@gmail.com>
2018-06-23 12:13:29 +0700

    perbaikan modul isset

---
+++ isset/isset.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+</body>
+<?php
+	$prima=' READY ';
+
+	// fungsi php untuk pengecekan variabel (terisi / tidak)
+	if(isset($prima)){
+		echo 'Variabel telah di inisiasi';
+
+	}
+	else{
+		echo 'Variabel belum di inisiasi';
+	}
+
+
+	// CONTOH 2
+
+	// $NIP ="10112390";
+	// if(isset($_GET["NIP"])) {
+	// 	$NIP =$_GET["NIP"];
+	// 	echo $NIP;
+	// }
+	?>
+</html>
