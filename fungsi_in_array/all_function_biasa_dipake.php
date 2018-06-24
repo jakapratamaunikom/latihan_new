f84fa006eac26dae39f15288857e52ec4cb03399
Jaka Pratama <jakapratama204@gmail.com>
2018-06-24 21:38:49 +0700

    Modul Fungsi yang biasa dipake array

--- fungsi_in_array/all_function_biasa_dipake.php
+++ fungsi_in_array/all_function_biasa_dipake.php
@@ -2,8 +2,22 @@
 $keboard = array(
 	'Gaming','Programming','Cashier'
 );
+echo 'Array : '. implode(' - ',$keboard);
+echo '<hr>';
 // fungsi membalikan nilai dalam array 
 $balik =array_reverse($keboard);
 
-echo implode('',$balik);
+echo 'Array Reverse : '.implode(' , ',$balik).'<br>';
+echo '<br>';
+
+sort($keboard);
+echo 'Sort (Ascending) : '.implode(' ',$keboard);
+echo '<br>';
+
+// function in_array
+	// return value : 1 (true)
+	// return value : 0 (false)
+$cariArayKebord = in_array('asf', $keboard);
+echo 'Mencari kata "Programming dalam array diatas "';
+echo 'Function Pencarian Array (in_array) : '. $cariArayKebord;
 ?>
